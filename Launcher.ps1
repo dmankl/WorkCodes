22b27958d8e1ebfb07bf68c7ce4d763c244a3b0f
dmankl <66547321+dmankl@users.noreply.github.com>
2022-01-04 15:18:37 -0700

    Added Launcher

---
+++ Launcher.ps1
@@ -0,0 +1,12 @@
+$SCCM = 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\Microsoft.ConfigurationManagement.exe'
+$User = "pcc-domain\wadmin_" + $env:UserName
+$creds = Get-Credential -Credential $User
+$Title = "Program Launching"
+$Message = "What Program Do You Want To Run?"
+$Options = "&SCCM", "&AD"
+$DefaultChoice = 0
+$Result = $Host.UI.PromptForChoice($Title, $Message, $Options, $DefaultChoice)
+switch ($Result) {
+    0 { Start-Process $SCCM -Credential $creds }
+    1 { Start-Process “C:\Windows\System32\cmd.exe” -workingdirectory $PSHOME -Credential $creds -ArgumentList “/c dsa.msc” }
+}
